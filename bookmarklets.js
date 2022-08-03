d282c5088d78f45151acd0f9a12b1cbba61c8bb5
enzoy <enzoy0@gmail.com>
2022-08-03 18:48:00 +0900

    Update bookmarklets.js

--- bookmarklets.js
+++ bookmarklets.js
@@ -8,4 +8,17 @@ javascript:(function() { location.href="https://www.facebook.com/search/top/?q="
 javascript:(function() {ztempstr = ""; for (i=0;i<95;i++) { ztempstr += "\u3000"; console.log(ztempstr); }; copy(ztempstr);})()
 
 //Find DOM classes and set display style (mainly, "none" to remove)
-javascript:(function() { var classesToFind = ["classname1", "classname2", "classname3"]; var displayToSet = prompt("display css:"); var loopcnt1 = classesToFind.length; for (var i = 0; i < loopcnt1; i++) { var tofind = document.getElementsByClassName(classesToFind[i]); var loopcnt2 = tofind.length; for (var j = 0; j < loopcnt2; j++) { tofind[j].style.display = displayToSet; }} })()
+javascript:(function() { var classesToFind = ["classname1", "classname2", "classname3"]; var displayToSet = prompt("style.display:="); var loopcnt1 = classesToFind.length; for (var i = 0; i < loopcnt1; i++) { var tofind = document.getElementsByClassName(classesToFind[i]); var loopcnt2 = tofind.length; for (var j = 0; j < loopcnt2; j++) { tofind[j].style.display = displayToSet; }} })()
+
+javascript:(function() {
+  var classesToFind = ["classname1", "classname2", "classname3"]; 
+  var displayToSet = prompt("style.display:="); 
+  var loopcnt1 = classesToFind.length; 
+  for (var i = 0; i < loopcnt1; i++) { 
+    var tofind = document.getElementsByClassName(classesToFind[i]); 
+    var loopcnt2 = tofind.length; 
+    for (var j = 0; j < loopcnt2; j++) { 
+      tofind[j].style.display = displayToSet; 
+    }
+  } 
+})()
